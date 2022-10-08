5ae5caa47df8589e8af86d5a9dffe5a2207f49a6
Reginaldo Leme <reginaldoleme2018@gmail.com>
2022-10-08 14:32:46 -0300

    Trabalhando com CAST e Constantes

---
+++ my-jobs/src/edu/reginaldo/tipos-variaveis/VariableTypes.java
@@ -0,0 +1,16 @@
+public class VariableTypes {
+    public static void main(String[] args) throws Exception {
+      
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 =  (short) numeroNormal;
+
+        int numero = 1;
+
+        final double VALOR_DE_PI = 3.14;
+
+    }
+
+}
